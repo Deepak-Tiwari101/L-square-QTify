1bfcb80ccba642b53ee910fa9635a27e25aeb404
Deepak-Tiwari101 <tiwarideepak.1199@gmail.com>
2024-01-21 12:13:08 +0530

    C3: Button

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FeedbackBtn from './components/FeedbackBtn/FeedbackBtn';
+
+function App() {
+  return (
+    <>
+      <FeedbackBtn />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/FeedbackBtn/FeedbackBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './FeedbackBtn.module.css';
+
+export default function FeedbackBtn() {
+    return (
+        <div>
+            <button className={styles.feedbackBtn}>
+                Give Feedback
+            </button>
+        </div >
+    )
+}
